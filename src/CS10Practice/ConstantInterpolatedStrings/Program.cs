041e807c80533eb903129e61bd51cbb1dc51def7
is12t <love.ms.is12t@hotmail.co.jp>
2021-10-31 23:11:52 +0900

    Add projects

---
+++ src/CS10Practice/ConstantInterpolatedStrings/Program.cs
@@ -0,0 +1,6 @@
+﻿public class C
+{
+    const string S1 = $"Hello world";
+    const string S2 = $"Hello{" "}World";
+    const string S3 = $"{S1} Kevin, welcome to the team!";
+}
---
+++ src/CS10Practice/LambdaImprovements/Program.cs
@@ -0,0 +1,14 @@
+﻿MapAction([HttpPost("/")] ([FromBody] string todo) => todo);    // ラムダで属性が使える
+MapAction(string (todo) => todo);   // 直接各ラムダで return の型ががける
+
+//MapAction([HttpPost("/")] todo => todo);    // これはエラー。ラムダに属性をつける場合、引数リストは ( ) が必要
+MapAction([HttpPost("/")] (todo) => todo);  // これは ( ) があるので OK
+
+// 関数やクラスの定義
+
+// Function
+void MapAction(Func<string, string> action) {; }
+
+// 属性
+class HttpPostAttribute : Attribute { public HttpPostAttribute(string root) {; } }
+class FromBodyAttribute : Attribute { public FromBodyAttribute() {; } }
